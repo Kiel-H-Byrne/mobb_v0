06d38837f460e54220e53d3c23306549d1c6714f
KHB <kiel.byrne@gmail.com>
2016-08-03 18:04:46 -0400

    added schema

--- imports/startup/collections/listings.js
+++ imports/startup/collections/listings.js
@@ -1,3 +1,5 @@
+//=================== NEW COLLECTION =========================
+
 Listings = new orion.collection('listings', {
   singularName: 'listing', // The name of one of these items
   pluralName: 'listings', // The name of more than one of these items
@@ -59,10 +61,105 @@ Listings = new orion.collection('listings', {
   }
 });
 
+//=================== COLLECTION SECURITY =========================
+
 Listings.allow({
   // update: function(userId, post) { return ownsDocument(userId, post); },
   // remove: function(userId, post) { return ownsDocument(userId, post); },
 });
+
+//=================== SCHEMAS =========================
+// https://github.com/aldeed/meteor-simple-schema
+
+Listings.attachSchema(new SimpleSchema({
+
+  // We use `label` to put a custom label for this form field
+  // Otherwise it would default to `Title`
+  // 'optional: false' means that this field is required
+  // If it's blank, the form won't submit and you'll get a red error message
+  // 'type' is where you can set the expected data type for the 'title' key's value
+  name: {
+    type: String,
+    optional: false,
+    unique: true,
+  },
+  address1: {
+    type: String,
+    max: 20,
+    optional: false
+  },
+  address2: {
+    type: String,
+    max: 20,
+    optional: true
+  },
+  address3: {
+    type: String,
+    max: 20,
+    optional: true,
+  },    
+  city: {
+    type: String,
+    max: 50,
+    optional: false
+  },
+  state: {
+    type: String,
+    regEx: /^A[LKSZRAEP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]$/,
+    optional: false
+  },
+  zip: {
+    type: String,
+    regEx: /^[0-9]{5}$/,
+    optional: false
+  },
+  country: {
+    type: String,
+    max: 3
+  },
+  url: {
+    type: String,
+    unique: true,
+    label: 'URL',
+    regEx: SimpleSchema.RegEx.Url
+    // custom: function () {
+    //   if (Meteor.isClient && this.isSet) {
+    //     Meteor.call("postWithSameLink", this.value, function (error, result) {
+    //       if (!result) {
+    //         Listings.simpleSchema().namedContext("postSubmitForm").addInvalidKeys([{name: "url", type: "notUnique"}]);
+    //       }
+    //     });
+    //   }
+    // }
+  },
+  phone: {
+    type: String,
+    max: 15
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date();
+      } else if (this.isUpsert) {
+        return {$setOnInsert: new Date()};
+      } else {
+        this.unset();
+      }
+    }
+  },
+  createdBy:{
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    optional: true,
+    index: 1,
+    autoValue: function(){
+      if ( this.userId && this.isInsert ) {
+        return this.userId;
+      }
+    }
+  }
+}));
 
 
 export default Listings;

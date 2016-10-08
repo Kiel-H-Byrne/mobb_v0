9d37945962cde2ae71cb4d2ccf1b0cea8f50286c
KHB <kiel.byrne@gmail.com>
2016-10-08 17:54:47 -0400

    .

--- imports/ui/layouts/nav.js
+++ imports/ui/layouts/nav.js
@@ -8,12 +8,30 @@ import '../components/infoModal.js';
 import './nav.html';
 
 
+
 Template.nav.onRendered( function() {
 // ====== MOBILE VIEW NAV MENU BUTTON, CLICKING SHOWS THE SIDE NAV. ====== 
 	$(document).ready(function(){
+
+		// lastX = window.innerWidth;
+		// lastY = window.innerHeight;
+
+		// function shadowOnResize() {
+		//    let x = window.innerWidth;
+		//    let y = window.innerHeight;
+		//    if (lastX >= 995 && 995 > x) {
+	 //        $('.button-collapse').sideNav('hide');
+		//    }
+		//    lastX = x;
+		//    lastY = y;
+		// };
+
+		// window.addEventListener("resize", shadowOnResize);
+
+
 		$(".button-collapse").sideNav({
-			closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    
+			edge: 'left',
+			closeOnClick: true
 		});
 
 	});

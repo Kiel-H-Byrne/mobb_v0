d0b1c732e6c37a4283b2eacd135d7d0a9ec94a37
KHB <kiel.byrne@gmail.com>
2017-07-29 20:48:41 -0400

    .

--- imports/ui/components/categorySelect.js
+++ imports/ui/components/categorySelect.js
@@ -5,15 +5,15 @@ import './categorySelect.html';
 
 Template.catSelect.onRendered(function () {
   $(document).ready(function () {
-    $('.tooltipped').tooltip();
+
     $('.dropdown-button').dropdown({
-      stopPropagation: true
+      // stopPropagation: true,
       // inDuration: 200,
       // outDuration: 225,
       // constrain_width: false, // Does not change width of dropdown to that of the activator
       // hover: false, // Activate on hover
       // gutter: 3, // Spacing from edge
-      // belowOrigin: true, // Displays dropdown below the button
+      // belowOrigin: false, // Displays dropdown below the button
       // alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
   });

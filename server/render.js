770ac0afc2a228ab3604d7c85e05a03a258bc1ed
KHB <kiel.byrne@gmail.com>
2017-08-26 19:19:25 -0400

    loadingspinner color change and change to myjoin page

--- server/render.js
+++ server/render.js
@@ -36,8 +36,8 @@ let styleTag = `
     height: 100%;
     border: 3px solid #00A466;
     border: 3px solid transparent;
-    border-top-color: #8104A0;
-    border-bottom-color: #8104A0;
+    border-top-color: #824691;
+    border-bottom-color: #824691;
     border-radius: 50%;
     -webkit-animation: loader 2s linear infinite;
     -moz-animation: loader 2s linear infinite;

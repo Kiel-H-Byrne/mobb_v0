76c49737aa4149a2dec030c0c991067411fbdf17
KHB <kiel.byrne@gmail.com>
2017-09-04 12:36:42 -0400

    success in implementing css-grid masonry but it forces widths of the cards, instead of laying them out responsively...

--- imports/ui/pages/test.js
+++ imports/ui/pages/test.js
@@ -9,10 +9,8 @@ const VTM = require('vanilla-text-mask/dist/vanillaTextMask.js');
 
 
 Template.test.onRendered(function () {
-  
     $(document).ready(function () {
       Materialize.updateTextFields();
-
       //mask is array of strings and regex
       let telMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
       let telInput = document.querySelector('#tmtest-tel');
@@ -20,16 +18,42 @@ Template.test.onRendered(function () {
         inputElement: telInput,
         mask: telMask
       });
-      
     });
-      
-  
 });
 
 
-// Template.test.helpers({
-//   randomDoc: function () {
-//     const doc =  Listings.findOne();
-//     return doc;
-//   }
-// });
+Template.test.helpers({
+  randomDoc: function () {
+    const doc =  Listings.findOne();
+    return doc;
+  },
+  testArray: function () {
+    let testArray = [];
+    for (i=0; i<=30; i++) {
+      testArray.push(i);
+    }
+    return testArray;
+  },
+  randoNumber: function() {
+    const min = 10
+    const max = 1000
+    const number = Math.floor(Math.random() * (max - min + 1)) + min; 
+    return number
+  },
+  favorites: function () {
+    // get the array of ids
+    let arr = Meteor.user().profile.favorites;
+    // for some reason the favorites array has one entry: 'NeQChWMre5Yh4ooBq'
+    // so it's nevery truly 'empty'; if greater than one, return list.
+    if (arr.length > 1) {
+      let cursor = Listings.find({
+        _id : {$in : arr}
+      }, {
+        sort: {name: 1, location: -1 }
+      });
+      return cursor;
+    } else {
+      return false;
+    }
+  }
+});

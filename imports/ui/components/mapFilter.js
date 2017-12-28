438e199b6a842c3af1745f6434e6aa5e4eae1207
KHB <kiel.byrne@gmail.com>
2017-12-27 23:31:33 -0500

    modified mapfilter toggleall

--- imports/ui/components/mapFilter.js
+++ imports/ui/components/mapFilter.js
@@ -18,24 +18,31 @@ Template.mapFilter.helpers({
 });
 
 Template.mapFilter.events({
-  'click .switch label': function(event,templateInstance) {
+  'click .switch label, click .main-switch label': function(event,templateInstance) {
     event.preventDefault();
   },
   'click .main-switch': function(event,templateInstance) {
+    // STOP PROPAGATING SO DROPDOWN DOESNT CLOSE
     event.stopPropagation();
-    const el = $(event.currentTarget.firstElementChild.children[0]);
-    if (el.prop('checked')) {
-      el.prop('checked', false);
-      //hide all markers
-    } else {
-      el.prop('checked', true);
-      //show all markers
+    const elArray = $('#filter_dropdown .switch input');
+    // console.log(elArray);
+
+    for (let i = 1, len = elArray.length; i < len; i++) {
+      const el = elArray[i];
+      if (el.checked) {
+        el.checked = false;
+        //hide all markers
+      } else {
+        el.checked = true;
+        //show all markers
+      }
     }
+
   },
   'click .cat_item .switch': function(event,templateInstance) {
     // event.preventDefault();
     event.stopPropagation();
-
+    //INPUT ELEMENT
     const el = $(event.currentTarget.firstElementChild.children[0]);
     if (el.prop('checked')) {
       el.prop('checked', false);
